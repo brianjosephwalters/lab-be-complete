be869d9c9d059983c405be6383e173235e7f07f9
Brian J. Walters <brian.joseph.walters@gmail.com>
2017-06-12 21:28:29 -0500

    Lab 5: Connected post manager to post resource accessor.

--- src/main/java/com/bjw/bloggit/managers/impl/PostManager.java
+++ src/main/java/com/bjw/bloggit/managers/impl/PostManager.java
@@ -3,42 +3,56 @@ package com.bjw.bloggit.managers.impl;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.bjw.bloggit.accessors.IPostAccessor;
 import com.bjw.bloggit.domains.Post;
 import com.bjw.bloggit.managers.IPostManager;
 
 @Component
 public class PostManager implements IPostManager {
 
+    @Autowired
+    private IPostAccessor postAccessor;
+    
     @Override
     public List<Post> getAllPosts() {
-        return Collections.emptyList();
+        return postAccessor.findAll();
     }
 
     @Override
     public Post getPostById(Long postId) {
-        return null;
+        return postAccessor.findOne(postId);
     }
 
     @Override
     public List<Post> getPostsByAuthor(String author) {
-        return Collections.emptyList();
+        return postAccessor.findAllByAuthor(author);
     }
 
     @Override
     public Post createPost(Post post) {
-        return null;
+        return postAccessor.save(post);
     }
 
     @Override
     public Post updatePost(Long postId, Post post) {
-        return null;
+        Post currentPost = postAccessor.findOne(postId);
+        if (currentPost == null || post.getPostId() != postId) {
+            return null;
+        }
+        return postAccessor.save(post);
     }
 
     @Override
     public Post deletePost(Long postId) {
-        return null;
+        Post post = postAccessor.findOne(postId);
+        if (post == null) {
+            return null;
+        }
+        postAccessor.delete(postId);
+        return post;
     }
 
 }
