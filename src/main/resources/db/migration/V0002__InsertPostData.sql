ba9fe8b6c8fea5244f6af8ca79a3a7acfdb28c96
Brian J. Walters <brian.joseph.walters@gmail.com>
2017-07-20 18:04:36 -0500

    Added more data.

--- src/main/resources/db/migration/V0002__InsertPostData.sql
+++ src/main/resources/db/migration/V0002__InsertPostData.sql
@@ -1,3 +1,13 @@
 insert into posts (title, body, last_updated, author) values
     ('A first post', 'This is the body of our first blog post.', '2017-05-01', 'Brian J. Walters'),
-    ('A second post', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan erat nec justo tempor, ut posuere nisl pretium. Morbi rutrum mauris eu turpis convallis iaculis. Nulla varius purus vitae feugiat venenatis. Proin vulputate augue nibh. Pellentesque facilisis elementum neque ut egestas. Nulla et tortor metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas massa orci, dapibus ut dolor sed, venenatis viverra risus.', '2017-05-02', 'Brian J. Walters');
+    ('A second post', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan erat nec justo tempor, ut posuere nisl pretium. Morbi rutrum mauris eu turpis convallis iaculis. Nulla varius purus vitae feugiat venenatis. Proin vulputate augue nibh. Pellentesque facilisis elementum neque ut egestas. Nulla et tortor metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas massa orci, dapibus ut dolor sed, venenatis viverra risus.', '2017-05-02', 'Brian J. Walters'),
+    ('A third post', 'Some text', '2017-04-30', 'Brian J. Walters'),
+    ('A fourth post', 'Some more text', '2017-04-15', 'Brian J. Walters'),
+    ('A fifth post', 'Some text', '2017-03-21', 'Brian J. Walters'),
+    ('A sixth post', 'Some text', '2017-04-10', 'Brian J. Walters'),
+    ('A seventh post', 'Some text', '2017-07-01', 'Brian J. Walters'),
+    ('A eighth post', 'Some text', '2017-05-10', 'Brian J. Walters'),
+    ('A nineth post', 'Some text', '2017-05-11', 'Brian J. Walters'),
+    ('A tenth post', 'Some text', '2017-01-21', 'Brian J. Walters'),
+    ('A eleventh post', 'Some text', '2016-03-12', 'Brian J. Walters'),
+    ('A twelth post', 'Some text', '2017-04-30', 'Brian J. Walters');
---
+++ src/test/java/com/bjw/bloggit/managers/impl/PostManagerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.bjw.bloggit.managers.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PostManagerIntegrationTest {
+
+    @Test
+    public void test() {
+    }
+
+}
